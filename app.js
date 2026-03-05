b03d3c28d99d2c7f49b60296bc98e151c23a3b0d
B1N4L <163591915+B1N4L@users.noreply.github.com>
2026-03-06 01:33:24 +0530

    chore: basic routes created - user and subscription

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 import express from "express";
 import {PORT} from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+
 const app = express();
 
-app.get("/", async (req, res) => {
-    res.send("Welcome to the Github App");
-})
+//show which routes which we want to use.
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.get('/', (req, res) => {
+    res.send({body:"welcome to api"});
+});
 
 app.listen(PORT, () => {
     console.log(`SubzTracker listening on port http://localhost:${PORT}`);
